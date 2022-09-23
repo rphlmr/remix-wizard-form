e934494d7839ed399ae7b70309117f6a91b30d3e
rphlmr <raphael@uaerom.com>
2022-09-24 00:07:49 +0200

    clean log

--- app/routes/wizard.tsx
+++ app/routes/wizard.tsx
@@ -3,7 +3,6 @@ import { redirect } from "@remix-run/node";
 import { commitWizardSession } from "~/sessions/wizard-session.server";
 
 export async function loader({ request }: LoaderArgs) {
-  console.log("init loader");
   // clear previous wizard session
   return redirect("wizard/step/1", {
     headers: {
